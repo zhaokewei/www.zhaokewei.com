c345a82baed266b8bc4f6d5924e13428d224816b
zhaokewei <zhaokewei1992@gmail.com>
2019-05-16 01:03:32 +0800

    first commit

---
+++ src/components/home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        友情链接: <a href="http://yaofan.zhaokewei.com">Surprise</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "home"
+    }
+</script>
+
+<style scoped>
+
+</style>
